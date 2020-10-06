8ab8505aa1d22dcbed28dd1327a4501bab9506cd
Maksym Baboshko <maksym.baboshko@yahoo.com>
2020-10-06 12:46:31 +0300

    Updates

---
+++ precache-manifest.c695d5e175aebf890c5509053f8acf26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be089fb00785b67e28f267132405bd1a",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "bd3a2871321394a12c4c",
+    "url": "/weather/static/css/main.fdb77ab0.chunk.css"
+  },
+  {
+    "revision": "230dd8b09fed172f0ed2",
+    "url": "/weather/static/js/2.d52f7ce9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather/static/js/2.d52f7ce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd3a2871321394a12c4c",
+    "url": "/weather/static/js/main.b9b66b06.chunk.js"
+  },
+  {
+    "revision": "cbd170559497437f72ba",
+    "url": "/weather/static/js/runtime-main.f775bfa1.js"
+  }
+]);
